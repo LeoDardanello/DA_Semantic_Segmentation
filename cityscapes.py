1523fc9a17caa10be1dae4e94363df279fa42cbd
Paola Matassa <127331331+PaolaMts@users.noreply.github.com>
2023-12-09 18:59:58 +0100

    fix cityscape

--- cityscapes.py
+++ cityscapes.py
@@ -4,48 +4,45 @@ from torch.utils.data import Dataset
 import os
 from PIL import Image
 
-def pil_loader(self):
-        with open(self.path, 'rb') as f:
+def pil_loader(p):
+        with open(p, 'rb') as f:
             img = Image.open(f)
             return img.convert('RGB')
 
-# mode = train or val, path = "/content/Cityscapes/Cityspaces/"  
+# mode = train or val, path = "/content/Cityscapes/Cityspaces/"
 class CityScapes(Dataset):
     def __init__(self, mode, path):
         super(CityScapes, self).__init__()
         self.path = path
         self.mode = mode
-        self.data, self.label = self.data_loader(mode, path)
+        self.data, self.label = self.data_loader()
 
 
     def __getitem__(self, idx):
-        image = pil_loader(self.path+"/"+self.data[idx])
-        label = pil_loader(self.path+"/"+self.label[idx])
+        image = pil_loader(self.path+self.data[idx])
+        label = pil_loader(self.path+self.label[idx])
         return image, label
 
     def __len__(self):
         return len(self.data)
-    
-    def data_loader(self, mode, folder_path):
+
+    def data_loader(self):
         data= []
         label = []
         types = ["images/", "gtFine/"]
         for t in types:
-            # Verifica se il percorso della cartella esiste
-            if os.path.exists(folder_path+t):
-                # Ottieni tutti i file nella cartella
-                for root, dirs, files in os.walk(folder_path+t):
-                    # Verifica se la cartella di destinazione è presente nella gerarchia del percorso
-                    if mode in root:
+            # check if path exists
+            if os.path.exists(self.path+t):
+                # get files from directory
+                for root, dirs, files in os.walk(self.path+t):
+                    if self.mode in root:
                         for file in files:
-                            # Crea il percorso completo del file
                             file_path = os.path.join(root, file)
-
-                            # Ottieni il percorso relativo alla cartella "train"
-                            relative_path = os.path.relpath(file_path, folder_path)
+                            # get path in mode
+                            relative_path = os.path.relpath(file_path, self.path)
                             if t=="images/":
                                 data.append(relative_path)
                             else:
                                 if file_path.split("gtFine_")[1] == "labelTrainIds.png":
                                     label.append(relative_path)
-        return data, label
+        return sorted(data), sorted(label)
