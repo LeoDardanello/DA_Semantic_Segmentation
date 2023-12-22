85e9aa49ee2bb237b6315b6a653b92f21c888e9f
PaolaMts <127331331+PaolaMts@users.noreply.github.com>
2023-12-22 22:00:18 +0100

    fix transformation cityscape

--- cityscapes.py
+++ cityscapes.py
@@ -4,33 +4,40 @@ from torch.utils.data import Dataset
 from torchvision import transforms
 import os
 from PIL import Image
-
-def pil_loader(p, mode):
-    with open(p, 'rb') as f:
-        img = Image.open(f)
-        return img.convert(mode)
+import numpy as np
+import torch
 
 # mode = train or val, path = "/content/Cityscapes/Cityspaces/"
 class CityScapes(Dataset):
-    def __init__(self, mode):
+    def __init__(self, mode,args):
         super(CityScapes, self).__init__()
         self.path = "/content/Cityscapes/Cityspaces/"
         self.mode = mode
         self.data, self.label = self.data_loader()
-        self.preprocess = transforms.Compose([
+        self.transform_data = transforms.Compose([ 
+            # transforms.Resize((args.crop_height,args.crop_width)), # Ridimensiona l'immagine
             transforms.ToTensor(),                 # Converte l'immagine in un tensore
+            transforms.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225))
         ])
-
+        self.width=args.crop_width
+        self.height = args.crop_height
+        
     def __getitem__(self, idx):
-        image = pil_loader(self.path+self.data[idx], 'RGB')
-        
-        label = pil_loader(self.path+self.label[idx], 'L')
-        tensor_image = self.preprocess(image)
-        
-        return tensor_image, self.preprocess(label)
+        image = self.pil_loader(self.data[idx],  'RGB')
+        label = self.pil_loader(self.label[idx],  'L')
+
+        tensor_image = self.transform_data(image)
+        tensor_label =torch.from_numpy(np.array(label))
+ 
+        return tensor_image, tensor_label
 
     def __len__(self):
         return len(self.data)
+    
+    def pil_loader(self, p, mode):
+        with open(self.path+p, 'rb') as f:
+            img = Image.open(f)
+            return img.convert(mode).resize((self.width, self.height), Image.NEAREST)
 
     def data_loader(self):
         data= []
