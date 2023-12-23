09bb68f6258200d6c6f68d9bb23d54d534769bb2
PaolaMts <127331331+PaolaMts@users.noreply.github.com>
2023-12-23 16:48:49 +0100

    fix gta

--- gta5.py
+++ gta5.py
@@ -9,86 +9,44 @@ from pprint import pprint
 from PIL import Image
 import numpy as np
 
-def pil_loader(p, mode):
-    with open(p, 'rb') as f:
-        img = Image.open(f)
-        return img.convert(mode)
-
-CityscapesClass = namedtuple('CityscapesClass', ['name', 'id', 'train_id', 'category', 'category_id',
-                                                     'has_instances', 'ignore_in_eval', 'color'])
-
-classes = [
-        CityscapesClass('unlabeled',            0, 255, 'void', 0, False, True, (0, 0, 0)),
-        CityscapesClass('ego vehicle',          1, 255, 'void', 0, False, True, (0, 0, 0)),
-        CityscapesClass('rectification border', 2, 255, 'void', 0, False, True, (0, 0, 0)),
-        CityscapesClass('out of roi',           3, 255, 'void', 0, False, True, (0, 0, 0)),
-        CityscapesClass('static',               4, 255, 'void', 0, False, True, (0, 0, 0)),
-        CityscapesClass('dynamic',              5, 255, 'void', 0, False, True, (111, 74, 0)),
-        CityscapesClass('ground',               6, 255, 'void', 0, False, True, (81, 0, 81)),
-        CityscapesClass('road',                 7, 0, 'flat', 1, False, False, (128, 64, 128)),
-        CityscapesClass('sidewalk',             8, 1, 'flat', 1, False, False, (244, 35, 232)),
-        CityscapesClass('parking',              9, 255, 'flat', 1, False, True, (250, 170, 160)),
-        CityscapesClass('rail track',           10, 255, 'flat', 1, False, True, (230, 150, 140)),
-        CityscapesClass('building',             11, 2, 'construction', 2, False, False, (70, 70, 70)),
-        CityscapesClass('wall',                 12, 3, 'construction', 2, False, False, (102, 102, 156)),
-        CityscapesClass('fence',                13, 4, 'construction', 2, False, False, (190, 153, 153)),
-        CityscapesClass('guard rail',           14, 255, 'construction', 2, False, True, (180, 165, 180)),
-        CityscapesClass('bridge',               15, 255, 'construction', 2, False, True, (150, 100, 100)),
-        CityscapesClass('tunnel',               16, 255, 'construction', 2, False, True, (150, 120, 90)),
-        CityscapesClass('pole',                 17, 5, 'object', 3, False, False, (153, 153, 153)),
-        CityscapesClass('polegroup',            18, 255, 'object', 3, False, True, (153, 153, 153)),
-        CityscapesClass('traffic light',        19, 6, 'object', 3, False, False, (250, 170, 30)),
-        CityscapesClass('traffic sign',         20, 7, 'object', 3, False, False, (220, 220, 0)),
-        CityscapesClass('vegetation',           21, 8, 'nature', 4, False, False, (107, 142, 35)),
-        CityscapesClass('terrain',              22, 9, 'nature', 4, False, False, (152, 251, 152)),
-        CityscapesClass('sky',                  23, 10, 'sky', 5, False, False, (70, 130, 180)),
-        CityscapesClass('person',               24, 11, 'human', 6, True, False, (220, 20, 60)),
-        CityscapesClass('rider',                25, 12, 'human', 6, True, False, (255, 0, 0)),
-        CityscapesClass('car',                  26, 13, 'vehicle', 7, True, False, (0, 0, 142)),
-        CityscapesClass('truck',                27, 14, 'vehicle', 7, True, False, (0, 0, 70)),
-        CityscapesClass('bus',                  28, 15, 'vehicle', 7, True, False, (0, 60, 100)),
-        CityscapesClass('caravan',              29, 255, 'vehicle', 7, True, True, (0, 0, 90)),
-        CityscapesClass('trailer',              30, 255, 'vehicle', 7, True, True, (0, 0, 110)),
-        CityscapesClass('train',                31, 16, 'vehicle', 7, True, False, (0, 80, 100)),
-        CityscapesClass('motorcycle',           32, 17, 'vehicle', 7, True, False, (0, 0, 230)),
-        CityscapesClass('bicycle',              33, 18, 'vehicle', 7, True, False, (119, 11, 32)),
-        CityscapesClass('license plate',        -1, 255, 'vehicle', 7, False, True, (0, 0, 142)),
-    ]
-
-train_id_to_color = [[c.train_id]+list(c.color) for c in classes]
-train_id_to_color = np.array(train_id_to_color)
-
 def one_hot_it(label, label_info):
-
 	semantic_map = np.zeros(label.shape[1:])
 	for index, info in enumerate(label_info):
 		color = info[-3:]
 		class_map = np.all(label == color.reshape(3, 1, 1), axis=0)
 		semantic_map[class_map] = index
 	pprint(semantic_map)
-	return torch.tensor(semantic_map)
+	return torch.from_numpy(semantic_map)
 
 
 class Gta5(Dataset):
-    def __init__(self, mode):
+    def __init__(self, mode, args):
         super(Gta5, self).__init__()
         self.path = "/content/GTA5/"
         self.mode = mode
         self.data, self.label = self.data_loader()
-        self.preprocess = transforms.Compose([
-            transforms.ToTensor(),                 
+        self.transform_data = transforms.Compose([ 
+            # transforms.Resize((args.crop_height,args.crop_width)), # Ridimensiona l'immagine
+            transforms.ToTensor(),                 # Converte l'immagine in un tensore
+            transforms.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225))
         ])
+        self.width=args.crop_width
+        self.height = args.crop_height
 
     def __getitem__(self, idx):
-        image = pil_loader(self.path+self.data[idx], 'RGB')
-        label = pil_loader(self.path+self.label[idx], 'RGB')
-        tensor_image = self.preprocess(image)
-        transform2= transforms.PILToTensor()
-        tensor_label = one_hot_it(transform2(label).numpy(), train_id_to_color)   
+        image = self.pil_loader(self.data[idx], 'RGB')
+        label = self.pil_loader(self.label[idx], 'RGB')
+        tensor_image = self.transform_data(image)
+        tensor_label = one_hot_it(np.array(label), train_id_to_color)   
         return tensor_image, tensor_label 
 
     def __len__(self):
         return len(self.data)
+    
+    def pil_loader(self, p, mode):
+        with open(self.path+p, 'rb') as f:
+            img = Image.open(f)
+            return img.convert(mode).resize((self.width, self.height), Image.NEAREST)
 
     def data_loader(self):
         data= []
